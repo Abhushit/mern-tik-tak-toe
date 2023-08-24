fc0bb5233b22ebddcda2b977ae2a58a205c24fde
Abhushit Chaudhary <chaudhary.abhushit@gmail.com>
2023-08-24 16:01:12 +0545

    production changes

--- server/client/src/pages/HomePage/index.tsx
+++ server/client/src/pages/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import GameList from '../../components/GameList'
 import PlayerProps from '../../types/PlayerType';
 
--- server/server.js
+++ server/server.js
@@ -40,6 +40,19 @@ app.use(function (err, req, res, next) {
   });
 });
 
+//production
+if (process.env.NODE_ENV === "production") {
+  const path = require("path");
+  app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+          if(err) {
+              res.status(500).send(err)
+          }
+      });
+  })
+}
+
 app.listen(app.get("PORT"), function (err, done) {
   if (err) {
     console.log("Server failed to run");
